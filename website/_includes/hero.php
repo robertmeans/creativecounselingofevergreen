0a109e8721b7b1b38908294659a9fa1c2e7aa17f
Evergreen Web Design <info@evergreenwebdesign.com>
2019-03-08 13:12:49 -0700

    banner updates
    changed title & background in hero.php

--- website/_includes/hero.php
+++ website/_includes/hero.php
@@ -1,7 +1,8 @@
 <section id="hero">
 	<div class="hero-logo">
-		<!-- <img src="_images/creative-counseling-of-evergreen-logo.png" alt="Creative Counseling of Evergreen logo"> -->
-		<img src="_images/logo-testing.png" alt="Creative Counseling of Evergreen logo">
+		<!-- <img src="_images/logo-testing.png" alt="Creative Counseling of Evergreen logo"> -->
+		<h1>Creative Counseling of Evergreen</h1>
+		<h2>Children, Teens and Adults</h2>
 	</div>
 	<a class="scroll-link" href="#services"><i class="fas fa-caret-down"></i></a>
 </section>
