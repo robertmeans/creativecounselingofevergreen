bbc914ef32c1e07bf0dde041207428661402f513
Evergreen Web Design <info@evergreenwebdesign.com>
2019-04-18 13:01:51 -0600

    added google verification to head

--- website/_includes/head.php
+++ website/_includes/head.php
@@ -21,6 +21,7 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta name="description" content="ABC Company description here...">
 	<meta name="format-detection" content="telephone=no">
+	<meta name="google-site-verification" content="v8bNPlRjjcUrw1zxAB-0lrxEWcWhcnWNt3RXLLXnWWc" />
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans|Bad+Script' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css">
